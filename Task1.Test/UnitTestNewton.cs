aa3bc41f25b6cc3bb826740d61edf1c9dcadfe84
olegpopok <olegpopo1994@gmail.com>
2015-07-13 00:35:07 +0300

    1

---
+++ Task1.Test/UnitTestNewton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1.Library;
+
+namespace Task1.Test
+{
+    [TestClass]
+    public class UnitTestNewton
+    {
+        [TestMethod]
+        public void RadicalTest()
+        {
+            double epsilon = 0.0001;
+
+            double value = Newton.Radical(8, 3, epsilon);
+
+            Assert.AreEqual(2, value, epsilon);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RadicalErorTest()
+        {
+            Newton.Radical(-16, 4, 0.01);
+        }
+    }
+}
